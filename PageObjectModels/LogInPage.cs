92c7fc043d719e66d72fc579ed601d01d44b4a2f
YusraaPeerun <88275191+YusraaPeerun@users.noreply.github.com>
2022-09-07 14:24:58 +0400

    Add files via upload

--- PageObjectModels/LogInPage.cs
+++ PageObjectModels/LogInPage.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.DevTools.V102.Page;
+using OpenQA.Selenium.Support.UI;
 
 namespace eShopOnWeb.Test.Drivers
 {
     public class LogInPage
     {
-        public string loginUrl = "https://localhost:44315/Identity/Account/Login";      
+        public string loginUrl = "https://localhost:44315/Identity/Account/Login";
 
-        public LogInPage NavigateToLogInPg(IWebDriver driver) {
+        public LogInPage NavigateToLogInPg(IWebDriver driver)
+        {
             driver.Navigate().GoToUrl(loginUrl);
             return this;
         }
 
-        public LogInPage enterEmail(IWebDriver driver,string email)
+        public LogInPage enterEmail(IWebDriver driver, string email)
         {
             driver.FindElement(By.Id("Input_Email")).SendKeys(email);
             return this;
         }
 
-        public LogInPage enterPassword(IWebDriver driver,string password)
+        public LogInPage enterPassword(IWebDriver driver, string password)
         {
             driver.FindElement(By.Id("Input_Password")).SendKeys(password);
             return this;
@@ -34,6 +38,37 @@ namespace eShopOnWeb.Test.Drivers
         {
             driver.FindElement(By.CssSelector(".btn-default")).Click();
             return this;
+        }
+
+        public void checkErrorMsg(IWebDriver driver, String ErrorMsg)
+        {
+
+            ReadOnlyCollection<IWebElement> currentErrorMsg = driver.FindElements(By.XPath("//div[@class='text-danger validation-summary-errors']/ul/li"));
+            int num = currentErrorMsg.Count();
+            if (currentErrorMsg.Count.Equals(1) || ErrorMsg.Contains("Email"))
+            {
+                Assert.AreEqual(ErrorMsg, currentErrorMsg[0].Text);
+            }
+            else if (ErrorMsg.Contains("Password"))
+            {
+                Assert.AreEqual(ErrorMsg, currentErrorMsg[1].Text);
+            }
+        }
+
+        public void checkMsgBelowFieldEmail(IWebDriver driver, String ErrorMsg)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var currentErrorMsg = wait.Until(driver =>
+                driver.FindElement(By.XPath("/html/body/div/div/div/div/section/form/div[2]/span/span")));
+            Assert.AreEqual(ErrorMsg, currentErrorMsg.Text);
+        }
+
+        public void checkMsgBelowFieldPasswordl(IWebDriver driver, String ErrorMsg)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var currentErrorMsg = wait.Until(driver =>
+                driver.FindElement(By.XPath("/html/body/div/div/div/div/section/form/div[3]/span/span")));
+            Assert.AreEqual(ErrorMsg, currentErrorMsg.Text);
         }
     }
 }
---
+++ PageObjectModels/MainPage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eShopOnWeb.Test.Drivers;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace eShopOnWeb.Test.PageObjectModels
+{
+    public class MainPage 
+    {
+        public string MainpgUrl = "https://localhost:44315/";
+
+        public MainPage NavigateToMainPg(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(MainpgUrl);
+            return this;
+        }
+
+        public void CheckTitle(IWebDriver driver)
+        {
+            Assert.AreEqual("Catalog - Microsoft.eShopOnWeb", driver.Title);
+        }
+
+        public MainPage clickOnLogInBtn(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("body > div > header > div > article > section.col-lg-4.col-md-5.col-xs-12 > div > section > div > a")).Click();
+            return this;
+     
+        }
+
+        public MainPage changeBrand(IWebDriver driver,String brand)
+        {
+            IWebElement selectBrand = driver.FindElement(By.Id("CatalogModel_BrandFilterApplied"));
+            SelectElement brandOption = new SelectElement(selectBrand);
+            brandOption.SelectByText(brand);
+            driver.FindElement(By.ClassName("esh-catalog-send")).Click();
+            return this;
+
+        }
+
+        public MainPage AddProductInTheBasket(ReadOnlyCollection<IWebElement> itemsOnScreen, String itemToAddInBasket)
+        {
+            for (int countItem = 1; countItem <= itemsOnScreen.Count; countItem++)
+            {
+                String currentText = (itemsOnScreen[countItem]).FindElement(By.XPath("form/div[1]/span")).Text;
+                if (currentText.Equals(itemToAddInBasket))
+                {
+                    Assert.AreEqual(currentText, itemToAddInBasket);
+                    itemsOnScreen[countItem].FindElement(By.XPath("form/input[1]")).Click();
+                    break;
+
+                }
+            }
+            return this;
+
+        }
+
+        public MainPage addMultipleProductsInTheBasket(ReadOnlyCollection<IWebElement> itemsOnScreen, IWebDriver driver, int numberOfProducts)
+        {
+            for (int i = 1; i <= numberOfProducts; i++)
+            {
+                itemsOnScreen[i].FindElement(By.XPath("form/input[1]")).Click();
+                updateQuantity(driver,"1");
+                updateBtn(driver);
+                if (i < numberOfProducts)
+                {
+                    driver.FindElement(By.XPath("/html/body/div/div/form/div/div[3]/section[1]/a")).Click();
+                }
+            }
+            return this;
+        }
+
+        public MainPage updateQuantity(IWebDriver driver, string quantity)
+        {
+            driver.FindElement(By.ClassName("esh-basket-input")).Clear();
+            driver.FindElement(By.ClassName("esh-basket-input")).SendKeys(quantity);
+            return this;
+        }
+
+        public MainPage updateBtn(IWebDriver driver)
+        {
+            driver.FindElement(By.Name("updatebutton")).Click();
+            return this;
+        }
+
+        public MainPage checkoutBtn(IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("/html/body/div/div/form/div/div[3]/section[2]/a")).Click();
+            return this;
+        }
+    }
+}
